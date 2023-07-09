6b8130ee37532e25d5b4c8ccbe2935a4a73aad0e
typosbro <khasanjonovich@gmail.com>
2023-07-10 04:54:25 +0900

    Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,41 @@ class _RootPageState extends State<RootPage> {
       appBar: AppBar(
         title: Text('App Bar Text $count'),
       ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        // items: const <BottomNavigationBarItem>[
+        //   BottomNavigationBarItem(
+        //     icon: Icon(Icons.home),
+        //     label: 'Home',
+        //     backgroundColor: Colors.blue,
+        //   ),
+        //   BottomNavigationBarItem(
+        //     icon: Icon(Icons.person),
+        //     label: 'Profile',
+        //     backgroundColor: Colors.green,
+        //   ),
+        // ],
+        // selectedItemColor: Colors.amber[800],
+        onDestinationSelected: (int index) {
+          debugPrint("Tapped on item $index");
+          setState(() {
+            count = index;
+          });
+        },
+        selectedIndex: count,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _incrementCount();
+          // _incrementCount();
           debugPrint("pressed");
         },
         child: const Icon(Icons.add),
