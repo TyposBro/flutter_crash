a965b3166dc632dffa56a869afcd80e3d111287e
typosbro <khasanjonovich@gmail.com>
2023-07-10 06:21:50 +0900

    learn flutter page

---
+++ lib/learn_flutter.page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter Page'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          Image.asset("images/sweden.jpg"),
+        ],
+      )),
+    );
+  }
+}
